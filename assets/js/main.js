a69783e1c9a9646effe560decf24b71159ea7495
Muh. Wira <muhwira907@gmail.com>
2023-01-23 13:25:03 +0800

    Popular Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,6 @@ const navToggle = document.getElementById("nav-toggle"),
     navMenu = document.getElementById("nav-menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("top-14");
     navMenu.classList.toggle("top-[-1000%]");
+    navMenu.classList.toggle("top-20");
 });
